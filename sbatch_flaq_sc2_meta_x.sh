c2fb42593fcf5114a1546105a3a75096185223f8
ybdong919 <yibo.dong@flhealth.gov>
2024-10-17 11:52:34 -0400

    combine lowdepth script and normal depth script to one

---
+++ sbatch_flaq_sc2_meta_x.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=bphl-umbrella
+#SBATCH --qos=bphl-umbrella
+#SBATCH --job-name=flaq_sc2_meta_x
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=15
+#SBATCH --mem=100gb
+#SBATCH --time=3-00
+#SBATCH --output=flaq_sc2_meta_x.%j.out
+#SBATCH --error=flaq_sc2_meta_x.%j.err
+
+module load apptainer
+
+#Run script/command and use $SLURM_CPUS_ON_NODE
+
+primers="4.1"
+
+python flaq_sc2_meta_x.py fastqs_ww/ --primer_bed primers/ARTIC-V${primers}.bed --lib_frag frag --threads $SLURM_CPUS_ON_NODE --ref_fasta reference/nCoV-2019.reference.fasta --ref_gff reference/GCF_009858895.2_ASM985889v3_genomic.gff --depthcutoff 10
